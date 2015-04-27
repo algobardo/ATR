06db7b745dd958c79ccf0dbbb98f3a95b90ce3b2
christofferqa <christofferqa@gmail.com>
2015-04-27 16:52:11 +0200

    Components

--- RemoteTestInterface/src/main/java/dk/au/cs/thor/rti/rtinterface/SchedulerInterface.java
+++ RemoteTestInterface/src/main/java/dk/au/cs/thor/rti/rtinterface/SchedulerInterface.java
@@ -1,7 +1,9 @@
 package dk.au.cs.thor.rti.rtinterface;
 
+import java.util.List;
+
 public interface SchedulerInterface {
     public void ready();
 
-    public void atInjectionSite(String viewAction);
+    public void atInjectionSite(String viewAction, String[] activeNeutralComponentNames);
 }
--- RemoteTestInterface/src/main/java/dk/au/cs/thor/rti/rtinterface/SchedulerTestInterface.java
+++ RemoteTestInterface/src/main/java/dk/au/cs/thor/rti/rtinterface/SchedulerTestInterface.java
@@ -1,5 +1,11 @@
 package dk.au.cs.thor.rti.rtinterface;
 
+import java.util.List;
+
 public interface SchedulerTestInterface {
     public void start();
+
+    public void loadNeutralComponents(String[] neutralComponentNames);
+
+    public void executeNeutralComponents(String[] neutralComponentNames);
 }
